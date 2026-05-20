da25be1cf83a27118acac4ae203df83c0817d0f2
AswinVmm <aswina002@gmail.com>
2026-05-20 11:47:59 +0530

    prisma changes

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,5 +1,20 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis;
+
+const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        // log: ["query"], // optional (debug)
+    });
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
