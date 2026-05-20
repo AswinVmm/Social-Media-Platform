1031eab1fe045b9801f7944bca3e2bf8bf1e6112
AswinVmm <aswina002@gmail.com>
2026-05-20 12:02:08 +0530

    prisma

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,20 +1,29 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // const prisma = new PrismaClient();
 
 // export default prisma;
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis;
+// const globalForPrisma = globalThis;
 
-const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        // log: ["query"], // optional (debug)
-    });
+// const prisma =
+//     globalForPrisma.prisma ||
+//     new PrismaClient({
+//         // log: ["query"], // optional (debug)
+//     });
 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== "production") {
+//     globalForPrisma.prisma = prisma;
+// }
+
+// export default prisma;
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 
 export default prisma;
